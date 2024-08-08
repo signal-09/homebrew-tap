1516ff0e3084a72c1abfa818095f359d1a6cfc54
Ettore Simone <ettore.simone@gmail.com>
2024-08-08 02:00:47 +0200

    Upgraded iproute4mac to 0.2.1

--- Formula/iproute4mac.rb
+++ Formula/iproute4mac.rb
@@ -3,9 +3,9 @@ class Iproute4mac < Formula
 
   desc "iproute for Mac"
   homepage "https://github.com/signal-09/iproute4mac"
-  version "0.2.0"
-  url "https://files.pythonhosted.org/packages/3f/23/4e90b20364d686647044cf2d7e573f6444e0e7bee8366b5af2ba1c4fa656/iproute4mac-0.2.0.tar.gz"
-  sha256 "89e8a07b1493bb567f73c091260f7fa952d6212c4bbe01da0639fa03667fe111"
+  version "0.2.1"
+  url "https://files.pythonhosted.org/packages/0a/b3/f851a6742aef125dd5219268cc170a0a5a1e6a6ce33e1bacd6300afc5225/iproute4mac-0.2.1.tar.gz"
+  sha256 "f252544845e9cba386ddbaa44af03d422c0fa62971bafaf262d1603c6803d7a5"
   head "https://github.com/signal-09/iproute4mac.git", branch: "master"
 
   depends_on :macos
