0bf26c0c72cd0b638d8199901ecfcf1e29348a05
Ettore Simone <ettore.simone@gmail.com>
2024-08-29 11:22:18 +0200

    Upgraded iproute4mac to 0.4.1

--- Formula/iproute4mac.rb
+++ Formula/iproute4mac.rb
@@ -2,15 +2,15 @@ class Iproute4mac < Formula
   include Language::Python::Virtualenv
 
   desc "CLI wrapper for iproute2 Linux routing utilities"
-  homepage "https://github.com/signal-09/iproute4mac"
-  url "https://files.pythonhosted.org/packages/fc/b3/31749bf2ffff2b7eaf20fd8e03430354e9dbfcefc53894a4367aae297c47/iproute4mac-0.4.0.tar.gz"
-  sha256 "a02253fffb4debea3e2bd493aecbc0881fe8357afa32418ee0d4b13db9d7c949"
-  head "https://github.com/signal-09/iproute4mac.git", branch: "master"
+  homepage "https://pypi.org/project/iproute4mac/"
+  url "https://files.pythonhosted.org/packages/cd/32/b344b2fc63a870b31e6ffc4e652cab186689441daba80e0a62eca4b25bb0/iproute4mac-0.4.1.tar.gz"
+  sha256 "05b07c3aa531967b3b51f23b490c4f81fffd36cf8d406cf7caf95d6356023799"
+  license "Apache-2.0"
 
   depends_on :macos
   depends_on "python@3.12"
 
-  conflicts_with "iproute2", "iproute2mac", because: "both provide same executables"
+  conflicts_with "iproute2mac", because: "both provide same executables"
 
   def install
     virtualenv_install_with_resources
