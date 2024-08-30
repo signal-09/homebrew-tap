ebf1c8422d3388c43a335e42bba0797e0eb543cd
Ettore Simone <signal-09@users.noreply.github.com>
2024-08-30 10:38:32 +0000

    Upgraded iproute4mac to 0.4.1

--- Formula/iproute4mac.rb
+++ Formula/iproute4mac.rb
@@ -3,7 +3,7 @@ class Iproute4mac < Formula
 
   desc "CLI wrapper for iproute2 Linux routing utilities"
   homepage "https://pypi.org/project/iproute4mac/"
-  url "https://files.pythonhosted.org/packages/cd/32/b344b2fc63a870b31e6ffc4e652cab186689441daba80e0a62eca4b25bb0/iproute4mac-0.4.1.tar.gz"
+  url "https://pypi.io/packages/source/i/iproute4mac/iproute4mac-0.4.1.tar.gz"
   sha256 "05b07c3aa531967b3b51f23b490c4f81fffd36cf8d406cf7caf95d6356023799"
   license "Apache-2.0"
 
